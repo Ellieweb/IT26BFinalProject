74aa038af5527815a5a6c98392f4e56eaf88b3be
Elliweb <tudtudleanne@gmail.com>
2026-04-02 19:44:23 +0800

    project fanalization

--- src/it26bfinalproject/CRUDdashboard.java
+++ src/it26bfinalproject/CRUDdashboard.java
@@ -322,6 +322,21 @@ public class CRUDdashboard extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(() -> new CRUDdashboard().setVisible(true));
     }
+    
+        private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField jTextField3;
+    private javax.swing.JTextField jTextField4;
+    private javax.swing.JTextField jTextField6;
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     // End of variables declaration//GEN-END:variables
